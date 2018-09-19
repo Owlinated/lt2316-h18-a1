2ccd46755f9ddcef0a2f088125dbcb5cd1aab34c
Unknown <florian@stellbr.ink>
2018-09-19 22:50:45 +0200

    Detect and handle attributes with continuous values

---
+++ helper.py
@@ -0,0 +1,9 @@
+# Itertools extension from https://docs.python.org/3/library/itertools.html
+from itertools import tee
+
+
+def pairwise(iterable):
+    "s -> (s0,s1), (s1,s2), (s2, s3), ..."
+    a, b = tee(iterable)
+    next(b, None)
+    return zip(a, b)
--- myid3.py
+++ myid3.py
@@ -2,11 +2,14 @@
 from math import log2
 from pickle import dump, load
 from typing import IO, List, Union, Iterator
-from pandas import DataFrame, Series, concat
+
+from numpy import int64, float64
+from pandas import DataFrame, Series
 from pandas.core.groupby import GroupBy
 from sklearn.metrics import precision_score, recall_score, accuracy_score, f1_score, confusion_matrix
 
-from matcher import DiscreteMatcher
+from helper import pairwise
+from matcher import DiscreteMatcher, BaseMatcher, ContinuousMatcher
 from node import BaseNode, Leaf, Node
 
 
@@ -24,19 +27,19 @@ class DecisionTree:
             self.root = load(load_from)
 
     @staticmethod
-    def __build_tree(X: DataFrame, y: Series, attributes: List[str], value: object = None) -> BaseNode:
+    def __build_tree(X: DataFrame, y: Series, attributes: List[str], matcher: BaseMatcher = None) -> BaseNode:
         assert X.shape[0] == y.shape[0] and X.shape[0] > 0
 
         # Check for leaf: Only one class remains
         class_groups: GroupBy = y.groupby(y)
         classes = list(class_groups.groups.keys())
         if len(class_groups.groups) == 1:
-            return Leaf(DiscreteMatcher(value), classes[0])
+            return Leaf(matcher, classes[0])
 
         # Check for no attributes left
         popular_class = sorted(class_groups, key=lambda key_value: key_value[1].size)[-1][0]
         if not any(True for _ in attributes):
-            return Leaf(DiscreteMatcher(value), popular_class)
+            return Leaf(matcher, popular_class)
 
         def entropy(set: Series) -> float:
             """
@@ -45,30 +48,73 @@ class DecisionTree:
             probs = map(lambda label: len(list(filter(lambda entry: entry == label, set))) / set.size, classes)
             return -sum(map(lambda prob: prob * log2(prob) if prob > 0 else 0, probs))
 
-        def gain(attr: str) -> float:
+        def gain(subsets: Iterator[Series]):
             """
-            Measures the information gain of an attribute
+            Measures the information gain of splitting into subsets
             """
-            subsets = map(lambda val: y[X[attr] == val], set(X[attr]))
             return entropy(y) - sum(map(lambda subset: subset.size / X.size * entropy(subset), subsets))
 
+        def discrete_gain(attr: str) -> float:
+            """
+            Measures the information gain of splitting into all values at an attribute
+            """
+            return gain(map(lambda val: y[X[attr] == val], set(X[attr])))
+
+        def continuous_gain(attr: str, threshold: float) -> float:
+            """
+            Measure the information gain of splitting along a threshold at an attribute
+            """
+            return gain([y[X[attr] <= threshold], y[X[attr] > threshold]])
+
+        def attribute_gain(attr: str) -> float:
+            """
+            Measures the information gain based on the type of attribute
+            """
+            attr_type = X.dtypes[attr]
+            if attr_type == int64:
+                # Find highest gain achievable by splitting at all values
+                return discrete_gain(attr)
+            if attr_type == float64:
+                # Find highest gain achievable with any threshold
+                thresholds = map(lambda value: 0.5 * (value[0] + value[1]), pairwise(sorted(set(X[attr]))))
+                return max(map(lambda threshold: continuous_gain(attr, threshold), thresholds))
+            raise ValueError("Unknown type of attribute: {}".format(attr_type))
+
         # Find highest information gain attribute
-        attributes = sorted(attributes, key=gain)
+        attributes = sorted(attributes, key=attribute_gain)
         attribute = attributes[-1]
         child_attributes = attributes[:-1]
 
-        # Create children for all values
-        children: List[BaseNode] = []
-        for child_value in set(X[attribute]):
-            child_mask = X[attribute] == child_value
+        def generate_matchers() -> Iterator[BaseMatcher]:
+            """
+            Generates matchers for the chosen attribute
+            """
+            if X.dtypes[attribute] == int64:
+                # Create matchers for all values in discrete case
+                for child_value in set(X[attribute]):
+                    yield DiscreteMatcher(child_value)
+            else:
+                # Find threshold with highest information gain
+                thresholds = map(lambda value: 0.5 * (value[0] + value[1]), pairwise(sorted(set(X[attribute]))))
+                threshold = sorted(thresholds, key=lambda threshold: continuous_gain(attribute, threshold))[-1]
+
+                # Generate matchers for <= and > than threshold
+                for threshold_direction in [False, True]:
+                    yield ContinuousMatcher(threshold, threshold_direction)
+
+        def match_to_node(matcher: BaseMatcher) -> BaseNode:
+            """
+            Converts a matcher into a node
+            """
+            child_mask = matcher.is_match(X[attribute])
             child_X = X[child_mask]
             child_y = y[child_mask]
             if child_X.size == 0:
-                children.append(Leaf(DiscreteMatcher(child_value), popular_class))
+                return Leaf(matcher, popular_class)
             else:
-                children.append(DecisionTree.__build_tree(child_X, child_y, child_attributes, child_value))
+                return DecisionTree.__build_tree(child_X, child_y, child_attributes, matcher)
 
-        return Node(DiscreteMatcher(value), attribute, children, popular_class)
+        return Node(matcher, attribute, list(map(match_to_node, generate_matchers())), popular_class)
 
     def train(self, X: DataFrame, y: Series, attrs: List[str], prune: bool = False) -> None:
         """
